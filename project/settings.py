a29c8bcee6d6eecefbf474bdb83dd01218998bdb
Shivam-Mhetre <shivammhetre2002@gmail.com>
2026-01-31 15:00:37 +0530

    shivam demo

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from corsheaders.defaults import default_headers
+from corsheaders.defaults import default_headers 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
